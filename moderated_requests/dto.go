9ea02e324cec22483d751df9b89d59c6d78cffd4
Pantelis Vratsalis <pvratsalis@gmail.com>
2022-12-05 10:25:20 +0200

    custom kind validator

--- moderated_requests/dto.go
+++ moderated_requests/dto.go
@@ -1,6 +1,9 @@
 package moderated_requests
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/pkg/errors"
+)
 
 type Kind = string
 
@@ -19,11 +22,31 @@ const (
 type Payload = map[string]interface{}
 
 type ModeratedRequestData struct {
-	Kind Kind    `json:"kind" validate:"oneof=WORKSPACES_DELETE"`
+	Kind Kind    `json:"kind" validate:"kind"`
 	Data Payload `json:"data"`
 }
 
 func (s *ModeratedRequestData) Validate() error {
 	validate := validator.New()
+	err := validate.RegisterValidation("kind", kind)
+
+	if err != nil {
+		return errors.Wrap(err, "Failed to register custom validator")
+	}
+
 	return validate.Struct(s)
+}
+
+func kind(f validator.FieldLevel) bool {
+	validKinds := []string{
+		DeleteWorkspace,
+	}
+
+	for _, v := range validKinds {
+		if f.Field().String() == v {
+			return true
+		}
+	}
+
+	return false
 }
